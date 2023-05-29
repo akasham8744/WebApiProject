72cca69b9dcdb158eadab23531ebf9d2eaec6aff
omnie <omnie@OM81>
2023-05-29 19:10:27 +0530

    Add project files.

---
+++ ApiTestProject.Tests/Controllers/WebApiTest.cs
@@ -0,0 +1,25 @@
+﻿using ApiTestProject;
+using ApiTestProject.Controllers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Web.Mvc;
+
+namespace ApiTestProject.Tests.Controllers
+{
+    [TestClass]
+    public class WebApiTest
+    {
+        [TestMethod]
+        public void Index()
+        {
+            // Arrange
+            WebApiController controller = new WebApiController();
+
+            // Act
+            ViewResult result = controller.Index() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual("Home Page", result.ViewBag.Title);
+        }
+    }
+}
---
+++ ApiTestProject/Controllers/WebApiController.cs
@@ -0,0 +1,66 @@
+﻿using ApiTestProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ApiTestProject.Controllers
+{
+    
+    public class WebApiController : ApiController
+    {
+        IList<Student> Student = new List<Student>()
+        {
+            new Student()
+                {
+                    StudentId = 1, StudentName = "Mukesh Kumar", Address = "New Delhi", Course = "IT"
+                },
+                new Student()
+                {
+                    StudentId = 2, StudentName = "Banky Chamber", Address = "London", Course = "HR"
+                },
+                new Student()
+                {
+                    StudentId = 3, StudentName = "Rahul Rathor", Address = "Laxmi Nagar", Course = "IT"
+                },
+                new Student()
+                {
+                    StudentId = 4, StudentName = "YaduVeer Singh", Address = "Goa", Course = "Sales"
+                },
+                new Student()
+                {
+                    StudentId = 5, StudentName = "Manish Sharma", Address = "New Delhi", Course = "HR"
+                },
+        };
+        
+
+        [HttpGet]
+        [Route("api/student/names")]
+        public IList<Student> GetAllStudents1()
+        {
+            //Return list of all employees    
+            return Student;
+        }
+        public Student GetStudentDetails(int id)
+        {
+            //Return a single employee detail    
+            var Stu = Student.FirstOrDefault(e => e.StudentId == id);
+            if (Stu == null)
+            {
+                throw new HttpResponseException(Request.CreateResponse(HttpStatusCode.NotFound));
+            }
+            return Stu;
+        }
+        [HttpPost]
+        public IList<Student> GetStudentDetails(Student student)
+        {
+            //Return list of all employees with new entry
+            IList<Student> Student = new List<Student>();
+            Student.Add(student);
+            
+            return Student;
+        }
+    }
+}
